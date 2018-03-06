e95d3df091f8be5f7762c4efee019f4962363440
Ildikó Mag Mokus Horváth <horvath.ildiko.mm@gmail.com>
2018-03-06 20:31:56 +0100

    working version without styling

--- src/App.js
+++ src/App.js
@@ -1,18 +1,54 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import wiki from "wikijs";
+import "./App.css";
+import Map from "./Map";
+import Search from "./Search";
+import Header from "./Header";
+import places from "./places";
 
 class App extends Component {
+  state = {
+    filteredPlaces: places,
+    selectedPlace: null,
+    selectedPlaceSummary: ""
+  };
+  onSelectChange = event => {
+    this.setState({
+      selectedPlace: null,
+      filteredPlaces:
+        event.target.value === "all"
+          ? places
+          : places.filter(place => place.category === event.target.value)
+    });
+  };
+
+  setSelectedPlace = id => {
+    this.setState({
+      selectedPlace: id,
+      selectedPlaceSummary: ""
+    });
+    const place=places.filter(p=>p.id===id)[0];
+    place && wiki().page(place.wiki)
+    .then(i => i.mainImage()).then(summary => this.setState({selectedPlaceSummary:summary}))
+  };
+
   render() {
-    return (
+      return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Map
+          selectedPlaceSummary={this.state.selectedPlaceSummary}
+          setSelectedPlace={this.setSelectedPlace}
+          selectedPlace={this.state.selectedPlace}
+          filteredPlaces={this.state.filteredPlaces}
+        />
+        <Search
+          places={places}
+          filteredPlaces={this.state.filteredPlaces}
+          onSelectChange={this.onSelectChange}
+          setSelectedPlace={this.setSelectedPlace}
+          selectedPlace={this.state.selectedPlace}
+        />
+        <Header />
       </div>
     );
   }
